1938c90955cd7eaf1e138767c4c44a24ead8ad3a
unknown <38829219+deepklim@users.noreply.github.com>
2019-09-11 17:51:27 -0400

    Add index page

--- nerd_names_api/main_routes/routes.py
+++ nerd_names_api/main_routes/routes.py
@@ -1,8 +1,13 @@
 import os
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 from nerd_names_api.db import query_mysql
 
 main = Blueprint('main', __name__)
+
+
+@main.route('/')
+def index():
+	return render_template('index.html')
 
 
 @main.route('/names')
